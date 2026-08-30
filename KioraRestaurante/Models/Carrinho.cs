b319d65adf877ec051f305c0061cf34dcd62e146
Clayton Brito <clayton.brito115@gmail.com>
2026-08-30 03:37:47 -0300

    Feat: Criando as Models Carrinho e ItemCarrinho

---
+++ KioraRestaurante/Models/Carrinho.cs
@@ -0,0 +1,11 @@
+﻿//Um Carrinho possui um identificador, pertence a um Usuário e possui uma lista de itens.
+namespace KioraRestaurante.Models
+{
+    public class Carrinho
+    {
+        public int Id { get; set; }  //EF Core já entende que é uma chave primária apenas pelo "Id". Aqui determinamos o Id de cada carrinho.
+        public int UsuarioId { get; set; } //Chave estrangeira do Id de usuário. Relaciona esse carrinho a 1 usuário.
+        public Usuario Usuario { get; set; } //Carrinho tem um usuário. Carrinho pertence a um usuário. Pega as informações do usuário que está relacionado a esse carrinho com o Id.
+        public List<ItemCarrinho> Itens { get; set; } = new(); //Criando uma lista podendo conter varios ItemCarrinho dentro dela.
+    }
+}
---
+++ KioraRestaurante/Models/ItemCarrinho.cs
@@ -0,0 +1,14 @@
+﻿//Essa classe cria um Objeto com UM produto e sua Quantidade e relaciona a um Carrinho. 
+
+namespace KioraRestaurante.Models
+{
+    public class ItemCarrinho
+    {
+        public int Id { get; set; } //EF Core já entende que é uma chave primária apenas pelo "Id". 
+        public int CarrinhoId { get; set; } //Chave estrangeira do Id de carrinho. Relaciona essa classe a 1 carrinho.
+        public Carrinho Carrinho { get; set; } //ItemCarrinho tem um carrinho. ItemCarrinho pertence a um carrinho. Relaciona esse item a 1 carrinho utilizando o Id.
+        public int ProdutoId { get; set; } //Chave estrangeira do Id de produto. Relaciona 1 produto a essa classe.
+        public Produto Produto { get; set; } //ItemCarrinho tem um Produto. Produto pode pertencer a um ItemProduto. Pega as informações do produto que está relacionado a esse ItemCarrinho com o Id.
+        public int Quantidade { get; set; } //Quantidade de vezes que o produto foi adicionado.
+    }
+}
