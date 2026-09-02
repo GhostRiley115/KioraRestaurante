36633e87f67aa9654238f0ea4fb534c378d062da
OneBruno <ms2689992@gmail.com>
2026-09-02 11:38:20 -0300

    Chore: Configurando a Model Produto

--- KioraRestaurante/Models/Produto.cs
+++ KioraRestaurante/Models/Produto.cs
@@ -1,11 +1,30 @@
-﻿namespace KioraRestaurante.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KioraRestaurante.Models
 {
     public class Produto
     {
         // O EF Core sabe automaticamente que "Id" é a Chave Primária.
         public int Id { get; set; }
+        //Diz para o Ef core o máximo de caracteres possiveis
+        [Required]
+        [MaxLegth(100)]
+        public string? Nome { get; set; }
+
+        [MaxLength(500)]
+        public string? Descricao { get; set; }
+
+        // Cria uma coluna e evita que o provider do postgres mapeie o decimal como numeric
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Preco { get; set; }
+        public string? Imagem { get; set; }
+        public bool Disponivel { get; set; } = true;
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
 
         //Define que um produto pode estar em vários ItemProduto e cria uma navegação do produto para cada ItemCarrinho que ele esteja.
-        public List<ItemCarrinho> ItensCarrinho { get; set; } = new(); 
+        public List<ItemCarrinho> ItensCarrinho { get; set; } = new();
+
     }
 }
