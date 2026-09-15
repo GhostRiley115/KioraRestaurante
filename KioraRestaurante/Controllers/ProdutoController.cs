8622a22a330752d5a6a52db7c6aba4138c8d9d64
OneBruno <ms2689992@gmail.com>
2026-09-15 19:55:37 -0300

    Feat: Adicionando a controller produto

---
+++ KioraRestaurante/Controllers/ProdutoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KioraRestaurante.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
