1eac24a0573695ce1939206625883688ad3506db
igor <igorc7871@gmail.com>
2026-09-16 20:04:10 -0300

    Feat: Adicionando a Viewmodel CriarProduto

---
+++ KioraRestaurante/ViewModels/CriarProdutoViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace KioraRestaurante.ViewModels
+{
+    public class CriarProdutoViewModel
+    {
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        [MaxLength(100)]
+        public string Nome { get; set; } = null!;
+
+        [MaxLength(500)]
+        public string? Descricao { get; set; }
+        [Required(ErrorMessage = "Informe o preço.")]
+        [Range(typeof(decimal), "0.01", "9999999", ErrorMessage = "Preço inválido.")]
+
+        [MaxLength(500)]
+        public string? Imagem { get; set; }
+
+        [Required(ErrorMessage = "Selecione uma categoria.")]
+        public int Categoria { get; set; }
+
+        public List<SelectListItem> CategoriaDisponiveis { get; set; } = new();
+    }
+}
