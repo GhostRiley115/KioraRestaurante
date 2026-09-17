caa7de388ef2fa504edbc9da803b5f667fb7d820
Clayton Brito <clayton.brito115@gmail.com>
2026-09-17 18:00:36 -0300

    WIP: salvando alterações antes de atualizar com a main

---
+++ KioraRestaurante/Migrations/20260917205003_AdicionarChaveConfirmacaoPedido.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KioraRestaurante.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionarChaveConfirmacaoPedido : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Pedidos_UsuarioId",
+                table: "Pedidos");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ChaveConfirmacao",
+                table: "Pedidos",
+                type: "char(36)",
+                nullable: true,
+                collation: "ascii_general_ci");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pedidos_UsuarioId_ChaveConfirmacao",
+                table: "Pedidos",
+                columns: new[] { "UsuarioId", "ChaveConfirmacao" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Pedidos_UsuarioId_ChaveConfirmacao",
+                table: "Pedidos");
+
+            migrationBuilder.DropColumn(
+                name: "ChaveConfirmacao",
+                table: "Pedidos");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pedidos_UsuarioId",
+                table: "Pedidos",
+                column: "UsuarioId");
+        }
+    }
+}
